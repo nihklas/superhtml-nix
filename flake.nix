d38801a2f0040bc903c50c23f7e657357741c050
Niklas Koll <niklas@nkoll.de>
2024-12-28 13:23:15 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,26 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    zig-overlay,
   }:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
-        overlays = [
-          (final: prev: {
-            zigpkgs = zig-overlay.packages.${prev.system}."0.13.0";
-          })
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
         version = "v0.5.3";
       in
         with pkgs; {
